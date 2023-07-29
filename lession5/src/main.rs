34e39e991eba8893191026c02601f97b06c786e4
u20024804 <u20024804@gmail.com>
2023-07-29 16:17:20 +0800

    longer string

--- lession5/src/main.rs
+++ lession5/src/main.rs
@@ -1,7 +1,14 @@
 mod macros;
+use std::error::Error;
+use std::fs::File;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     println!("{}", longer_string!("hello", "world!"));
     println!("{}", longer_string!("hello", "world", "helloworld!"));
+    //println!("{}", longer_string!(1, 2, 3));
+    println!("{:?}", longer_string!(vec![1], vec![1,2], vec![1,2,3]));
     println!("Hello, world!");
+
+    let greeting_file: File = File::open("hello.txt")?;
+    Ok(())
 }
